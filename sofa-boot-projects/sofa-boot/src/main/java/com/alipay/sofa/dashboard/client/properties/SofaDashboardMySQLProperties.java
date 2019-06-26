11cbd5f4e8693b82f1f7088deb6ee202aca87b95
chpengzh <chpengzh@foxmail.com>
2019-06-26 16:55:08 +0800

    fix format error

--- sofa-boot-projects/sofa-boot/src/main/java/com/alipay/sofa/dashboard/client/properties/SofaDashboardMySQLProperties.java
+++ sofa-boot-projects/sofa-boot/src/main/java/com/alipay/sofa/dashboard/client/properties/SofaDashboardMySQLProperties.java
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.alipay.sofa.dashboard.client.properties;
 
 /**
@@ -5,15 +21,15 @@ package com.alipay.sofa.dashboard.client.properties;
  */
 public class SofaDashboardMySQLProperties {
 
-    private String           driver                   = "com.mysql.jdbc.Driver";
+    private String driver                   = "com.mysql.jdbc.Driver";
 
-    private String           url                      = "jdbc:mysql://127.0.0.1:3306/sofa_dashboard_client";
+    private String url                      = "jdbc:mysql://127.0.0.1:3306/sofa_dashboard_client";
 
-    private String           username                 = "root";
+    private String username                 = "root";
 
-    private String           password                 = "";
+    private String password                 = "";
 
-    private int              maximumActiveConnections = 10;
+    private int    maximumActiveConnections = 10;
 
     public String getDriver() {
         return driver;
--- sofa-boot-projects/sofa-boot/src/main/java/com/alipay/sofa/dashboard/client/properties/SofaDashboardZookeeperProperties.java
+++ sofa-boot-projects/sofa-boot/src/main/java/com/alipay/sofa/dashboard/client/properties/SofaDashboardZookeeperProperties.java
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.alipay.sofa.dashboard.client.properties;
 
 public class SofaDashboardZookeeperProperties {
@@ -10,22 +26,22 @@ public class SofaDashboardZookeeperProperties {
     /**
      * Zookeeper 客户端错误重试间隔(ms).
      */
-    private int baseSleepTimeMs = 1000;
+    private int    baseSleepTimeMs     = 1000;
 
     /**
      * Zookeeper 客户端最大重试次数.
      */
-    private int maxRetries = 3;
+    private int    maxRetries          = 3;
 
     /**
      * Zookeeper 客户端会话超时时间(ms).
      */
-    private int sessionTimeoutMs = 6000;
+    private int    sessionTimeoutMs    = 6000;
 
     /**
      * Zookeeper 客户端超时时间(ms).
      */
-    private int connectionTimeoutMs = 6000;
+    private int    connectionTimeoutMs = 6000;
 
     public String getAddress() {
         return address;
